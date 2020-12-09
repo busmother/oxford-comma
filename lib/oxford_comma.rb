c7d2b07b7fed9818c89b29635c66f013d8f935f2
Ian Marshall <imarshall14@gmail.com>
2020-12-09 18:59:56 -0500

    commit tree a3e400c8d830afe43264d4ff7067527a13328b92

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
-def oxford_comma(array)
+require "pry"
+#def oxford_comma(array)
+ #   new_array = []
+ #   case array.length
+ #   when array.length() == 1
+ #       new_array = array
+  #  when array.length() == 2
+  #      new_array = array.join(" and ")
+  #  else 
+ #       array.last = " and #{array.last}"
+ #       new_array = array.join(", ")
+ #   end new_array
+#end
 
+def oxford_comma(array)
+    new_array = []
+    if array.length() == 1
+        return array[0]
+    elsif array.length() == 2
+        new_array = array.join(" and ")
+        return new_array
+    else 
+        last_word = array.last
+        array[-1] = "and #{last_word}"
+        new_array = array.join(", ")
+        return new_array
+    end 
 end
